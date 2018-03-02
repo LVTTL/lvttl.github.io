899ae9c2b449789e40805f7a5c5c7d47e3b844b7
Moritz <LVTTL@users.noreply.github.com>
2018-03-03 00:02:49 +0100

    added readme based description to repo tiles

--- script.js
+++ script.js
@@ -17,23 +17,33 @@ function getRepos() {
             let title = document.createElement("h4");
             title.innerText = repo.name;
             child.appendChild(title);
+            getReadme(repo, child); 
             container.appendChild(child);
         });
-        repos.forEach (repo => {
-            getReadme(repo);
-        })
     });
 }
 
-function getReadme(repo) {
+function getReadme(repo, node) {
     fetch("https://api.github.com/repos/lvttl/"+repo.name+"/contents/README.md").then(response => {
         return response.json()
-    }).then(result => console.log(window.atob(result.content)));
+    }).then(result => {
+        let markdown = window.atob(result.content);
+        let lines = markdown.split('\n');
+        lines = lines.filter((val, i) => i > 0);
+        let lastRelevantLineNum = lines.findIndex((line, i) => line.charAt(0) == '#');
+        if (lastRelevantLineNum < 0) {
+            lastRelevantLineNum = lines.length;
+        }
+        //console.log(lastRelevantLineNum);
+        lines = lines.filter((line, i) => i < lastRelevantLineNum - 1);
+        //console.log(lines);
+        let description = lines.reduce((prev, cur, i, array) => prev + '\n' + cur);
+        //console.log(description);
+        let descNode = document.createElement("p");
+        descNode.innerText = description;
+        node.appendChild(descNode);
+    });
 }
-
-// get README.md
-
-
 
 window.addEventListener("load", evt => {
     getRepos();
